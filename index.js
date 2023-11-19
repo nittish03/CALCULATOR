71c5d86228c5f77ee585c2f8c136ee928801e117
nittish03 <nittishbaboria03@gmail.com>
2023-11-19 16:47:53 +0530

    did new js

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ function display(value){
     sv.value += value;
 }
 function clearvalue(){
-    sv.value = " ";
+    sv.value = "";
 }
 function calculate(){
     sv.value=eval(sv.value);
 }
+function backspace(){
+    
+}
+let nothing=document.getElementById("clear");
+nothing.addEventListener("click",clearvalue);
